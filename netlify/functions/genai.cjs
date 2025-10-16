acb739a404a58ae2e498b1af6a504bf4d6a2706b
Jesus Quijada <jesusdavid34553902@gmail.com>
2025-10-16 05:08:34 -0400

    Update genai.cjs

--- netlify/functions/genai.cjs
+++ netlify/functions/genai.cjs
@@ -22,7 +22,7 @@ exports.handler = async function (event) {
     }
 
     const genAI = new GoogleGenerativeAI(apiKey);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const fullPrompt = `
 Eres un generador experto de paquetes de software.
